00ab695eabc10f1f67e568778ca1e45c7b9bce15
vatsalpatel <vatsalpatel.me@gmail.com>
2023-02-28 15:37:11 +0530

    Generate report and save

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from celery.result import AsyncResult
 
-from worker import generate_report
+from worker import generate_report_task
+from db import postgres_client
 
 load_dotenv()
 app = FastAPI()
@@ -10,8 +11,7 @@ app = FastAPI()
 
 @app.post("/trigger_report")
 async def triggerReport():
-    task = generate_report.delay()
-    print(task.task_id)
+    task = generate_report_task.delay()
     return {"reportId": task.task_id}
 
 
@@ -19,6 +19,16 @@ async def triggerReport():
 async def getReport(request: Request):
     body = await request.json()
     report_id = body.get("reportId")
+
     res = AsyncResult(report_id)
-    print(report_id, res.ready())
-    return {"report": report_id, "state": res.ready()}
+    complete = res.ready()
+
+    if complete:
+        if res.result is not True:
+            return {"reportId": report_id, "status": "Complete", "success": False}
+        cursor = postgres_client.cursor()
+        cursor.execute(f"SELECT * FROM generated_reports WHERE report_id = '{report_id}'")
+        report = cursor.fetchone()[1]
+        return {"reportId": report_id, "status": "Complete", "report": report, "success": True}
+
+    return {"reportId": report_id, "status": "Running", "success": True}
--- worker.py
+++ worker.py
@@ -1,8 +1,11 @@
 import os
-from time import sleep
 
 from dotenv import load_dotenv
 from celery import Celery
+
+from db import redis_client, postgres_client
+from report import generate_report
+
 
 load_dotenv()
 
@@ -12,5 +15,17 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 
 @celery.task(bind=True)
-def generate_report(self):
+def generate_report_task(self):
+    report_id = self.request.id
+    redis_client.set(report_id, "RUNNING")
+
+    # Generate and store report here
+    report = generate_report()
+    report_to_save = "\n".join(report)
+    cursor = postgres_client.cursor()
+    cursor.execute(f"INSERT INTO generated_reports VALUES ('{report_id}', '{report_to_save}')")
+    postgres_client.commit()
+
+    # Set status in redis and return to handler
+    redis_client.set(report_id, "COMPLETE")
     return True
